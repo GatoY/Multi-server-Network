0604571acae9da73cc00d1914494f5685b8a7e0c
GatoY <derakhy@gmail.com>
2018-04-15 15:12:04 +1000

    skeleton code

---
+++ Distributed System/project/skeleton/src/activitystreamer/client/ClientSkeleton.java
@@ -0,0 +1,61 @@
+package activitystreamer.client;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import activitystreamer.util.Settings;
+
+public class ClientSkeleton extends Thread {
+	private static final Logger log = LogManager.getLogger();
+	private static ClientSkeleton clientSolution;
+	private TextFrame textFrame;
+	
+
+	
+	public static ClientSkeleton getInstance(){
+		if(clientSolution==null){
+			clientSolution = new ClientSkeleton();
+		}
+		return clientSolution;
+	}
+	
+	public ClientSkeleton(){
+		
+		
+		textFrame = new TextFrame();
+		start();
+	}
+	
+	
+	
+	
+	
+	
+	@SuppressWarnings("unchecked")
+	public void sendActivityObject(JSONObject activityObj){
+		
+	}
+	
+	
+	public void disconnect(){
+		
+	}
+	
+	
+	public void run(){
+
+	}
+
+	
+}
