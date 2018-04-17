9fa3ae312d11dd9c808d7ae22ce923171ecdda96
dong <dong@dongs-mbp.home>
2018-04-18 01:24:49 +1000

    specify Commands

---
+++ src/activitystreamer/util/Command.java
@@ -0,0 +1,22 @@
+package activitystreamer.util;
+
+public class Command {
+    public static final String AUTHENTICATE = "AUTHENTICATE";
+    public static final String INVALID_MESSAGE = "INVALID_MESSAGE";
+    public static final String AUTHTENTICATION_FAIL = "AUTHTENTICATION_FAIL";
+    public static final String LOGIN = "LOGIN";
+    public static final String LOGIN_SUCCESS = "LOGIN_SUCCESS";
+    public static final String REDIRECT = "REDIRECT";
+    public static final String LOGIN_FAILED = "LOGIN_FAILED";
+    public static final String LOGOUT = "LOGOUT";
+    public static final String ACTIVITY_MESSAGE = "ACTIVITY_MESSAGE";
+    public static final String SERVER_ANNOUNCE = "SERVER_ANNOUNCE";
+    public static final String ACTIVITY_BROADCAST = "ACTIVITY_BROADCAST";
+    public static final String REGISTER = "REGISTER";
+    public static final String REGISTER_FAILED = "REGISTER_FAILED";
+    public static final String REGISTER_SUCCESS = "REGISTER_SUCCESS";
+    public static final String LOCK_REQUEST = "LOCK_REQUEST";
+    public static final String LOCK_DENIED = "LOCK_DENIED";
+    public static final String LOCK_ALLOWED = "LOCK_ALLOWED";
+
+}
