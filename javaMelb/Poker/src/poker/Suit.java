d719ef7021883c7f291e09e6f102dbaafe5a0f10
GatoY <derakhy@gmail.com>
2018-04-17 14:17:26 +1000

    reset

--- javaMelb/Poker/src/poker/Suit.java
+++
@@ -1,7 +0,0 @@
-package poker;
-
-//Record all suits.
-public enum Suit {
-	
-	Clubs, Diamonds, Hearts, Spades
-}
--- pygithub/photoMagic/test.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-import p2m
-img1=cv2.imread('1.jpeg')
-img2=cv2.imread('2.jpeg')
-img=p2m.addweight(img1,img2,0.5,0.5)
-cv2.imshow('image',img)
-cv2.waitKey()
-cv2.imwrite('new.jpeg',img)
