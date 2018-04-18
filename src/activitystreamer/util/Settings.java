6fe20d64a9885a8a15c2a3614122e2fa10640ddd
Mason <ms.catmissyou@gmail.com>
2018-04-18 19:39:37 +1000

    -ms set server id

--- src/activitystreamer/util/Settings.java
+++ src/activitystreamer/util/Settings.java
@@ -6,104 +6,112 @@ import java.security.SecureRandom;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.util.Random; //ms
 
 public class Settings {
-	private static final Logger log = LogManager.getLogger();
-	private static SecureRandom random = new SecureRandom();
-	private static int localPort = 3780;
-	private static String localHostname = "localhost";
-	private static String remoteHostname = null;
-	private static int remotePort = 3780;
-	private static int activityInterval = 5000; // milliseconds
-	private static String secret = null;
-	private static String username = "anonymous";
-	// -yu, server id.
-	private static String serverId;
+    private static final Logger log = LogManager.getLogger();
+    private static SecureRandom random = new SecureRandom();
+    private static int localPort = 3780;
+    private static String localHostname = "localhost";
+    private static String remoteHostname = null;
+    private static int remotePort = 3780;
+    private static int activityInterval = 5000; // milliseconds
+    private static String secret = null;
+    private static String username = "anonymous";
+    // -yu, server id.
+    private static String serverId;
 
-	// -yu set server id.
-	public static void setServerId() {
-		return;
-	}
+    // -ms set server id.
+    public static void setServerId() {
+        String range = "0123456789abcdefghijklmnopqrstuvwxyz";
+        StringBuilder randomId = new StringBuilder();
+        //randomId.length = 26
+        for (int i = 0; i < 26; i++){
+            randomId.append(range.charAt(random.nextInt(range.length())));
+        }
+        serverId = randomId.toString();
+        return;
+    }
 
-	// -yu get server id.
-	public static String getServerId() {
-		return serverId;
-	}
+    // -yu get server id.
+    public static String getServerId() {
+        return serverId;
+    }
 
-	public static int getLocalPort() {
-		return localPort;
-	}
+    public static int getLocalPort() {
+        return localPort;
+    }
 
-	public static void setLocalPort(int localPort) {
-		if (localPort < 0 || localPort > 65535) {
-			log.error("supplied port " + localPort + " is out of range, using " + getLocalPort());
-		} else {
-			Settings.localPort = localPort;
-		}
-	}
+    public static void setLocalPort(int localPort) {
+        if (localPort < 0 || localPort > 65535) {
+            log.error("supplied port " + localPort + " is out of range, using " + getLocalPort());
+        } else {
+            Settings.localPort = localPort;
+        }
+    }
 
-	public static int getRemotePort() {
-		return remotePort;
-	}
+    public static int getRemotePort() {
+        return remotePort;
+    }
 
-	public static void setRemotePort(int remotePort) {
-		if (remotePort < 0 || remotePort > 65535) {
-			log.error("supplied port " + remotePort + " is out of range, using " + getRemotePort());
-		} else {
-			Settings.remotePort = remotePort;
-		}
-	}
+    public static void setRemotePort(int remotePort) {
+        if (remotePort < 0 || remotePort > 65535) {
+            log.error("supplied port " + remotePort + " is out of range, using " + getRemotePort());
+        } else {
+            Settings.remotePort = remotePort;
+        }
+    }
 
-	public static String getRemoteHostname() {
-		return remoteHostname;
-	}
+    public static String getRemoteHostname() {
+        return remoteHostname;
+    }
 
-	public static void setRemoteHostname(String remoteHostname) {
-		Settings.remoteHostname = remoteHostname;
-	}
+    public static void setRemoteHostname(String remoteHostname) {
+        Settings.remoteHostname = remoteHostname;
+    }
 
-	public static int getActivityInterval() {
-		return activityInterval;
-	}
+    public static int getActivityInterval() {
+        return activityInterval;
+    }
 
-	public static void setActivityInterval(int activityInterval) {
-		Settings.activityInterval = activityInterval;
-	}
+    public static void setActivityInterval(int activityInterval) {
+        Settings.activityInterval = activityInterval;
+    }
 
-	public static String getSecret() {
-		return secret;
-	}
+    public static String getSecret() {
+        return secret;
+    }
 
-	public static void setSecret(String s) {
-		secret = s;
-	}
+    public static void setSecret(String s) {
+        secret = s;
+    }
 
-	public static String getUsername() {
-		return username;
-	}
+    public static String getUsername() {
+        return username;
+    }
 
-	public static void setUsername(String username) {
-		Settings.username = username;
-	}
+    public static void setUsername(String username) {
+        Settings.username = username;
+    }
 
-	public static String getLocalHostname() {
-		return localHostname;
-	}
+    public static String getLocalHostname() {
+        return localHostname;
+    }
 
-	public static void setLocalHostname(String localHostname) {
-		Settings.localHostname = localHostname;
-	}
+    public static void setLocalHostname(String localHostname) {
+        Settings.localHostname = localHostname;
+    }
 
-	/*
-	 * some general helper functions
-	 */
+    /*
+     * some general helper functions
+     */
 
-	public static String socketAddress(Socket socket) {
-		return socket.getInetAddress() + ":" + socket.getPort();
-	}
+    public static String socketAddress(Socket socket) {
+        return socket.getInetAddress() + ":" + socket.getPort();
+    }
 
-	public static String nextSecret() {
-		return new BigInteger(130, random).toString(32);
-	}
+    public static String nextSecret() {
+        return new BigInteger(130, random).toString(32);
+    }
 
 }
