a3c794ce9f5ad00bd1a781b32d3cb86fd7a6b4d2
Mason <ms.catmissyou@gmail.com>
2018-04-21 16:46:54 +1000

    1. writeUTF to write 2. StringBuffer rejected 3. Disconnect will close jframe

--- src/activitystreamer/client/ClientSkeleton.java
+++ src/activitystreamer/client/ClientSkeleton.java
@@ -1,113 +1,88 @@
 package activitystreamer.client;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
+import activitystreamer.util.Settings;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import activitystreamer.util.Settings;
+import java.io.*;
+import java.net.Socket;
 
 public class ClientSkeleton extends Thread {
-	private static final Logger log = LogManager.getLogger();
-	private static ClientSkeleton clientSolution;
-	private TextFrame textFrame;
-	private Socket socket;
-	DataOutputStream dos;
-	DataInputStream dis;
-	InputStreamReader isr;
-	BufferedReader br;
-	public static ClientSkeleton getInstance(){
-		if(clientSolution==null){
-			clientSolution = new ClientSkeleton();
+    private static final Logger log = LogManager.getLogger();
+    private static ClientSkeleton clientSolution;
+    private TextFrame textFrame;
+    private Socket socket;
+    private DataOutputStream dos;
+    private InputStreamReader isr;
+    private BufferedReader br;
 
-		}
-		return clientSolution;
-	}
-	
-	public ClientSkeleton(){
-		try {
-			socket = new Socket(Settings.getRemoteHostname(), Settings.getRemotePort());
-			dos = new DataOutputStream(socket.getOutputStream());
-			dis = new DataInputStream(socket.getInputStream());
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		textFrame = new TextFrame();		
-		start();
-	}
-	
-	
-	
-	@SuppressWarnings("unchecked")
-	public void sendActivityObject(JSONObject activityObj){
-		try {
-			dos.writeUTF(activityObj.toJSONString() + "\n");
-			dos.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	
-	public void disconnect(){
-		try {
-			if (socket != null) {
-				socket.close();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	public void run(){	
-		String msg = "";
-		
-		try {
+    public static ClientSkeleton getInstance() {
+        if (clientSolution == null) {
+            clientSolution = new ClientSkeleton();
+        }
+        return clientSolution;
+    }
 
-			isr = new InputStreamReader(dis);
-			br = new BufferedReader(isr);
-			StringBuffer sb = new StringBuffer();
-			
-			while (true) {
-				msg = br.readLine();
-				if (msg == null) {
-					break;
-				}
-				sb.append(msg);
-			}
-			
-			JSONParser jp = new JSONParser();
-			JSONObject jo = new JSONObject();
-			jo = (JSONObject) jp.parse(sb.toString());
-			
-			textFrame.setOutputText(jo);
-			
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-						
-	}
+    public ClientSkeleton() {
+        try {
+            socket = new Socket(Settings.getRemoteHostname(), Settings.getRemotePort());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        textFrame = new TextFrame();
+        start();
+    }
+
+
+    @SuppressWarnings("unchecked")
+    public void sendActivityObject(JSONObject activityObj) {
+        try {
+            dos = new DataOutputStream(socket.getOutputStream());
+            PrintWriter out = new PrintWriter(dos, true);
+            out.write(activityObj.toJSONString() + "\n");
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect() {
+        try {
+            if (socket != null) {
+                textFrame.dispose();
+                br.close();
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        String msg;
+        try {
+            isr = new InputStreamReader(socket.getInputStream());
+            br = new BufferedReader(isr);
+
+            while (true) {
+                if (socket.isClosed()) {
+                    break;
+                }
+                msg = br.readLine();
+                if (msg == null) {
+                    break;
+                }
+                JSONParser jp = new JSONParser();
+                JSONObject jo = (JSONObject) jp.parse(msg);
+                textFrame.setOutputText(jo);
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
