201e7e93b5e316c3833f741f4e19e92426aa63cd
GatoY <derakhy@gmail.com>
2018-04-21 20:52:53 +1000

    announce

---
+++ src/activitystreamer/server/Control.java
@@ -0,0 +1,256 @@
+package activitystreamer.server;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+import activitystreamer.util.Message;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+
+import activitystreamer.util.Settings;
+
+public class Control extends Thread {
+	private static final Logger log = LogManager.getLogger();
+	private static ArrayList<Connection> connections;
+	private static boolean term = false;
+	private static Listener listener;
+
+	protected static Control control = null;
+	private static Connection interact;
+
+	public static Control getInstance() {
+		if (control == null) {
+			control = new Control();
+		}
+		return control;
+	}
+
+	private Control() {
+		// initialize the connections array
+		connections = new ArrayList<>();
+		// start a listener
+		try {
+			listener = new Listener();
+		} catch (IOException e1) {
+			log.fatal("failed to startup a listening thread: " + e1);
+			System.exit(-1);
+		}
+	}
+
+	public void initiateConnection() {
+		// make a connection to another server if remote hostname is supplied
+		if (Settings.getRemoteHostname() != null) {
+			try {
+				Connection interact = outgoingConnection(
+						new Socket(Settings.getRemoteHostname(), Settings.getRemotePort()));
+				Message.authenticate(interact);
+			} catch (IOException e) {
+				log.error("failed to make connection to " + Settings.getRemoteHostname() + ":"
+						+ Settings.getRemotePort() + " :" + e);
+				System.exit(-1);
+			}
+		}
+	}
+
+	/**
+	 * Processing incoming messages from the connection. Return true if the
+	 * connection should close.
+	 *
+	 * @param con
+	 * @param msg
+	 *            result JSON string
+	 * @return
+	 */
+	public synchronized boolean process(Connection con, String msg) {
+		JSONObject request;
+		try {
+			request = (JSONObject) new JSONParser().parse(msg);
+		} catch (Exception e) {
+			Message.invalidMsg(con, "the received message is not in valid format");
+			return true;
+		}
+
+		if (request.get("command") == null) {
+			Message.invalidMsg(con, "the received message did not contain a command");
+			return true;
+		}
+
+		String command = (String) request.get("command");
+
+		switch (command) {
+		case Message.INVALID_MESSAGE:
+			return true;
+		case Message.AUTHENTICATE:
+			if (request.get("secret") == null) {
+				Message.invalidMsg(con, "the received message did not contain a secret");
+			}
+			String secret = (String) request.get("secret");
+			if (!secret.equals(Settings.serverSecret)) {
+				// if the secret is incorrect
+				Message.authenticationFail(con, "the supplied secret is incorrect: " + secret);
+			} else if (Settings.isIsRemoteAuthenticated()) {
+				Message.invalidMsg(con, "the server has already successfully authenticated");
+			} else {
+				Settings.setIsRemoteAuthenticated(true);
+				// No reply if the authentication succeeded.
+			}
+			break;
+		case Message.AUTHENTICATION_FAIL:
+			return true;
+		case Message.LOGIN:
+			return !login(con, request);
+		case Message.LOGOUT:
+			return true;
+		case Message.ACTIVITY_MESSAGE:
+			if (!request.containsKey("username")) {
+				return Message.authenticationFail(con, "the message did not contain a username");
+			}
+			if (!request.containsKey("secret")) {
+				return Message.authenticationFail(con, "the message did not contain a secret");
+			}
+			String activityUsername = (String) request.get("username");
+			String activityPassword = (String) request.get("secret");
+			if (activityUsername.equals("anonymous")) {
+				if (activityUsername.equals(Settings.getUsername())) {
+					return broadcastActivity(this.getConnections(), (JSONObject) request.get("activity"));
+				}
+			} else {
+				if (!activityUsername.equals(Settings.getUsername())
+						|| !activityPassword.equals(Settings.getSecret())) {
+					return Message.authenticationFail(con, "the supplied secret is incorrect: " + activityPassword);
+				}
+				return broadcastActivity(this.getConnections(), (JSONObject) request.get("activity"));
+			}
+			return true;
+		}
+		return true;
+	}
+
+	public synchronized boolean login(Connection con, JSONObject request) {
+		JSONObject response = new JSONObject();
+		if (request.containsKey("username") && request.containsKey("password")) {
+			String username = (String) request.get("username");
+			String password = (String) request.get("password");
+			if (validate(username, password)) {
+				Settings.setUsername(username);
+				Settings.setSecret(password);
+				response.put("command", Message.LOGIN_SUCCESS);
+				con.writeMsg(response.toJSONString());
+				return true;
+			} else {
+				response.put("command", Message.LOGIN_FAILED);
+				response.put("info", "attempt to login with wrong secret");
+			}
+
+		} else {
+			response.put("command", Message.INVALID_MESSAGE);
+		}
+		con.writeMsg(response.toJSONString());
+		return false;
+	}
+
+	public boolean validate(String username, String password) {
+		return true;
+	}
+
+	private boolean broadcastActivity(List<Connection> connections, JSONObject activity) {
+		for (Connection c : connections) {
+			Message.activityBroadcast(c, activity);
+		}
+		return false;
+	}
+
+	/**
+	 * The connection has been closed by the other party.
+	 *
+	 * @param con
+	 */
+	public synchronized void connectionClosed(Connection con) {
+		if (!term) {
+			connections.remove(con);
+		}
+	}
+
+	/**
+	 * A new incoming connection has been established, and a reference is returned
+	 * to it
+	 *
+	 * @param s
+	 * @return
+	 * @throws IOException
+	 */
+	public synchronized Connection incomingConnection(Socket s) throws IOException {
+		log.debug("incoming connection: " + Settings.socketAddress(s));
+		Connection c = new Connection(s);
+		connections.add(c);
+		return c;
+
+	}
+
+	/**
+	 * A new outgoing connection has been established, and a reference is returned
+	 * to it
+	 *
+	 * @param s
+	 * @return
+	 * @throws IOException
+	 */
+	public synchronized Connection outgoingConnection(Socket s) throws IOException {
+		log.debug("outgoing connection: " + Settings.socketAddress(s));
+		Connection c = new Connection(s);
+		connections.add(c);
+		return c;
+
+	}
+
+	@Override
+	public void run() {
+		log.info("using activity interval of " + Settings.getActivityInterval() + " milliseconds");
+		if (interact != null) {
+			while (!term) {
+				// do something with 5 second intervals in between
+				Socket tem = interact.getSocket();
+				JSONObject announce = new JSONObject();
+				announce.put("command", "SERVER_ANNOUNCE");
+				announce.put("id", Settings.getServerId());
+				announce.put("load", connections.size());
+				announce.put("hostname", tem.getLocalAddress());
+				announce.put("port", tem.getLocalPort());
+				interact.writeMsg(announce.toJSONString());
+				try {
+					Thread.sleep(Settings.getActivityInterval());
+				} catch (InterruptedException e) {
+					log.info("received an interrupt, system is shutting down");
+					break;
+				}
+				if (!term) {
+					log.debug("doing activity");
+					term = doActivity();
+				}
+			}
+		}
+		log.info("closing " + connections.size() + " connections");
+		// clean up
+		for (Connection connection : connections) {
+			connection.closeCon();
+		}
+		listener.setTerm(true);
+	}
+
+	public boolean doActivity() {
+		return false;
+	}
+
+	public final void setTerm(boolean t) {
+		term = t;
+	}
+
+	public final ArrayList<Connection> getConnections() {
+		return connections;
+	}
+}
--- src/activitystreamer/util/Settings.java
+++ src/activitystreamer/util/Settings.java
@@ -10,121 +10,121 @@ import org.apache.logging.log4j.Logger;
 import java.util.Random; //ms
 
 public class Settings {
-    private static final Logger log = LogManager.getLogger();
-    private static SecureRandom random = new SecureRandom();
-    private static int localPort = 3780;
-    private static String localHostname = "localhost";
-    private static String remoteHostname = null;
-    private static int remotePort = 3780;
-    private static int activityInterval = 5000; // milliseconds
-    private static String secret = null;
-    private static String username = "anonymous";
-    // -yu, server id.
-    private static String serverId;
-    // -ms, serverIdLength
-    private static int serverIdLength = 26;
-    private static boolean isRemoteAuthenticated = false;
-    public static final String serverSecret = "gen1p85md2qnq0d59qll3fbcoa";
+	private static final Logger log = LogManager.getLogger();
+	private static SecureRandom random = new SecureRandom();
+	private static int localPort = 3780;
+	private static String localHostname = "localhost";
+	private static String remoteHostname = null;
+	private static int remotePort = 3780;
+	private static int activityInterval = 5000; // milliseconds
+	private static String secret = null;
+	private static String username = "anonymous";
+	// -yu, server id.
+	private static String serverId;
+	// -ms, serverIdLength
+	private static int serverIdLength = 26;
+	private static boolean isRemoteAuthenticated = false;
+	public static final String serverSecret = "gen1p85md2qnq0d59qll3fbcoa";
 
-    // -ms set server id.
-    public static void setServerId() {
-        String range = "0123456789abcdefghijklmnopqrstuvwxyz";
-        Random rd = new Random();
-        StringBuilder randomId = new StringBuilder();
-        //randomId.length = 26
-        for (int i = 0; i < serverIdLength; i++) {
-            randomId.append(range.charAt(rd.nextInt(range.length())));
-        }
-        serverId = randomId.toString();
-        return;
-    }
+	// -ms set server id.
+	public static void setServerId() {
+		String range = "0123456789abcdefghijklmnopqrstuvwxyz";
+		Random rd = new Random();
+		StringBuilder randomId = new StringBuilder();
+		// randomId.length = 26
+		for (int i = 0; i < serverIdLength; i++) {
+			randomId.append(range.charAt(rd.nextInt(range.length())));
+		}
+		serverId = randomId.toString();
+		return;
+	}
 
-    // -yu get server id.
-    public static String getServerId() {
-        return serverId;
-    }
+	// -yu get server id.
+	public static String getServerId() {
+		return serverId;
+	}
 
-    public static int getLocalPort() {
-        return localPort;
-    }
+	public static int getLocalPort() {
+		return localPort;
+	}
 
-    public static void setLocalPort(int localPort) {
-        if (localPort < 0 || localPort > 65535) {
-            log.error("supplied port " + localPort + " is out of range, using " + getLocalPort());
-        } else {
-            Settings.localPort = localPort;
-        }
-    }
+	public static void setLocalPort(int localPort) {
+		if (localPort < 0 || localPort > 65535) {
+			log.error("supplied port " + localPort + " is out of range, using " + getLocalPort());
+		} else {
+			Settings.localPort = localPort;
+		}
+	}
 
-    public static int getRemotePort() {
-        return remotePort;
-    }
+	public static int getRemotePort() {
+		return remotePort;
+	}
 
-    public static void setRemotePort(int remotePort) {
-        if (remotePort < 0 || remotePort > 65535) {
-            log.error("supplied port " + remotePort + " is out of range, using " + getRemotePort());
-        } else {
-            Settings.remotePort = remotePort;
-        }
-    }
+	public static void setRemotePort(int remotePort) {
+		if (remotePort < 0 || remotePort > 65535) {
+			log.error("supplied port " + remotePort + " is out of range, using " + getRemotePort());
+		} else {
+			Settings.remotePort = remotePort;
+		}
+	}
 
-    public static String getRemoteHostname() {
-        return remoteHostname;
-    }
+	public static String getRemoteHostname() {
+		return remoteHostname;
+	}
 
-    public static void setRemoteHostname(String remoteHostname) {
-        Settings.remoteHostname = remoteHostname;
-    }
+	public static void setRemoteHostname(String remoteHostname) {
+		Settings.remoteHostname = remoteHostname;
+	}
 
-    public static int getActivityInterval() {
-        return activityInterval;
-    }
+	public static int getActivityInterval() {
+		return activityInterval;
+	}
 
-    public static void setActivityInterval(int activityInterval) {
-        Settings.activityInterval = activityInterval;
-    }
+	public static void setActivityInterval(int activityInterval) {
+		Settings.activityInterval = activityInterval;
+	}
 
-    public static String getSecret() {
-        return secret;
-    }
+	public static String getSecret() {
+		return secret;
+	}
 
-    public static void setSecret(String s) {
-        secret = s;
-    }
+	public static void setSecret(String s) {
+		secret = s;
+	}
 
-    public static String getUsername() {
-        return username;
-    }
+	public static String getUsername() {
+		return username;
+	}
 
-    public static void setUsername(String username) {
-        Settings.username = username;
-    }
+	public static void setUsername(String username) {
+		Settings.username = username;
+	}
 
-    public static String getLocalHostname() {
-        return localHostname;
-    }
+	public static String getLocalHostname() {
+		return localHostname;
+	}
 
-    public static void setLocalHostname(String localHostname) {
-        Settings.localHostname = localHostname;
-    }
+	public static void setLocalHostname(String localHostname) {
+		Settings.localHostname = localHostname;
+	}
 
-    /*
-     * some general helper functions
-     */
+	/*
+	 * some general helper functions
+	 */
 
-    public static String socketAddress(Socket socket) {
-        return socket.getInetAddress() + ":" + socket.getPort();
-    }
+	public static String socketAddress(Socket socket) {
+		return socket.getInetAddress() + ":" + socket.getPort();
+	}
 
-    public static String nextSecret() {
-        return new BigInteger(130, random).toString(32);
-    }
+	public static String nextSecret() {
+		return new BigInteger(130, random).toString(32);
+	}
 
-    public static boolean isIsRemoteAuthenticated() {
-        return isRemoteAuthenticated;
-    }
+	public static boolean isIsRemoteAuthenticated() {
+		return isRemoteAuthenticated;
+	}
 
-    public static void setIsRemoteAuthenticated(boolean isRemoteAuthenticated) {
-        Settings.isRemoteAuthenticated = isRemoteAuthenticated;
-    }
+	public static void setIsRemoteAuthenticated(boolean isRemoteAuthenticated) {
+		Settings.isRemoteAuthenticated = isRemoteAuthenticated;
+	}
 }
